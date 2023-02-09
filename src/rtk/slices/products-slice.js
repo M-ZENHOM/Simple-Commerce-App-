f2eb42d71d69159faefe33b4e3958dd3ef33f597
ZENHOM <you@example.com>
2023-02-10 01:30:07 +0200

    update fetch fnc

--- src/rtk/slices/products-slice.js
+++ src/rtk/slices/products-slice.js
@@ -2,10 +2,15 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export const fetchProducts = createAsyncThunk(
   "productsSlice/fetchProducts",
-  async () => {
-    const res = await fetch("https://dummyjson.com/products");
-    const data = await res.json();
-    return data.products;
+  async (_, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+    try {
+      const res = await fetch("https://dummyjson.com/products");
+      const data = await res.json();
+      return data.products;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
   }
 );
 
